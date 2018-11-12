aed86c2eb6d05caf1e189ce0cbc3d7bd372e6b2d
渡邉輝 <w.hikaru218sekaino10th@i.softbank.jp>
2018-11-12 19:49:11 +0900

    Initial Commit

---
+++ HYOI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HYOI
+//
+//  Created by 渡邉輝 on 2018/11/12.
+//  Copyright © 2018 渡邉輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ HYOITests/HYOITests.swift
@@ -0,0 +1,34 @@
+//
+//  HYOITests.swift
+//  HYOITests
+//
+//  Created by 渡邉輝 on 2018/11/12.
+//  Copyright © 2018 渡邉輝. All rights reserved.
+//
+
+import XCTest
+@testable import HYOI
+
+class HYOITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
